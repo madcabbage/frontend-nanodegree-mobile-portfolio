b5afba09c77ab95078bc9ee6e4a3b6b48542b36a
madcabbage <madcabbage@gmail.com>
2015-06-16 22:28:49 +0800

    reduced file sizes etc and added grunt
    Added grunt to resizes images
    Added google analyics
    Deferred CSS
    Minified CSS and JS

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      micro: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width:'100',
+            quality:'50',
+            name:'100'
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img_src/',
+          dest: 'img/'
+        }]
+      }
+
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['img'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['img/'],
+        },
+      },
+    },
+
+   copy: {
+      dev: {
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['img_src/fixed/*'],
+          dest: 'img/'
+        }]
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images', 'copy']);
+
+};
